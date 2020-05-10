0339e37c99e9f367213484e38f2a84aa6e95208d
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 05:07:57 -0300

    fix: routes precisa receber uma propriedade 'isSignedIn' para movero o usuário para sua rota

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,35 +8,40 @@ import Home from './pages/Home';
 
 const Stack = createStackNavigator();
 
-export default () => {
-
+export default ({ isSignedIn = false }) => {
+    const stackNavigationOption = {
+        headerTransparent: false,
+        headerTintColor: "#FFF",
+        headerTitleStyle: {
+            fontWeight: "bold",
+        },
+        headerTitleAlign: "center",
+        headerStyle: {
+            backgroundColor: "#312e38"
+        },
+        headerLeftContainerStyle: {
+            marginLeft: 20,
+        }
+    }
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerTransparent: false,
-                    headerTintColor: "#FFF",
-                    headerTitleStyle: {
-                        fontWeight: "bold",
-                    },
-                    headerTitleAlign: "center",
-                    headerStyle: {
-                        backgroundColor: "#312e38"
-                    },
-                    headerLeftContainerStyle: {
-                        marginLeft: 20,
-                    }
-                }}>
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{
-                        headerTransparent: true,
-                        title: ''
-                    }}
-                />
-                <Stack.Screen name="Home" component={Home} />
-            </Stack.Navigator>
+            {isSignedIn ? (
+                <Stack.Navigator screenOptions={stackNavigationOption}>
+                    <Stack.Screen name="Home" component={Home} />
+                </Stack.Navigator>
+            ) : (
+                    <Stack.Navigator screenOptions={stackNavigationOption}>
+                        <Stack.Screen
+                            name="Login"
+                            component={Login}
+                            options={{
+                                headerTransparent: true,
+                                title: ''
+                            }}
+                        />
+                    </Stack.Navigator>
+                )
+            }
         </NavigationContainer>
     )
 }
