37622f3a118b4a75ff3af73a48e3532f32f681fb
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 04:40:47 -0300

    fix: adicionando provider e o persistGate no App

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import { store, persistor } from './src/store';
 
 import Routes from './src/routes';
 
 export default function Index() {
     return (
-        <>
-            <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-            <Routes />
-        </>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+                <Routes />
+            </PersistGate>
+        </Provider>
     )
 }
 
