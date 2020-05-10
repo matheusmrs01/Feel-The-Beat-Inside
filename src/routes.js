2972de6d4c1fc99929f166312b7d5195ec48797c
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 03:03:05 -0300

    feat: criação do arquivo de rotas

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+const Stack = createStackNavigator();
+
+export default () => {
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
