70160784df81a75f4d8811521ed1a3d892dbcf8a
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 03:38:01 -0300

    fix: estilo padrão da status bar

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 
 import Routes from './src/routes';
 
 export default function Index() {
-    return <Routes />
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+            <Routes />
+        </>
+    )
 }
 
