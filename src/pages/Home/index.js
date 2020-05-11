e7228260b0ce9a35653624e9fba0b558a8be7cd3
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 23:53:01 -0300

    fix: toda vez que entrar na page inicial é verificado a validade do token, se for invalido é atualizado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { refreshToken, logout } from '../../store/modules/auth/actions';
+import refreshSpotifyTokens from '../../utils/refreshTokens';
 
 // import { Container } from './styles';
 
-const Home = ({ navigation }) => {
+export default function Home({ navigation }) {
+    const dispatch = useDispatch();
+    const refreshTokenVariable = useSelector(state => state.auth.refresh_token);
+    const tokenExpirationTime = useSelector(state => state.auth.expires_in)
+
+    async function requestNewTokens() {
+        let response
+        try {
+            response = await refreshSpotifyTokens(refreshTokenVariable)
+        } catch (err) {
+            dispatch(logout())
+            return
+        }
+
+        const { access_token, expires_in, refresh_token } = response;
+        const expirationTime = new Date().getTime() + expires_in * 1000;
+
+        dispatch(refreshToken(access_token, expirationTime, refresh_token))
+        return
+    }
+
+    useEffect(() => {
+        if (!tokenExpirationTime || new Date().getTime() > tokenExpirationTime) {
+            requestNewTokens()
+        }
+    }, [])
+
     return (
         <TouchableOpacity onPress={() => { navigation.navigate('Login') }}>
             <Text>Go to login</Text>
         </TouchableOpacity>
     );
 }
-
-export default Home;
