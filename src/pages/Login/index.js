c2b63bb1f88ae583b31b8bc9e5b564cb1063bc0d
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-10 22:47:33 -0300

    fix: login agora salva os dados vindo do spotify no redux

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useDispatch } from 'react-redux';
+
+import { signInSuccess } from '../../store/modules/auth/actions';
+import getToken from '../../utils/getTokens';
 
 import Background from '../../components/Background';
 
 import { Container, TouchableContainer, Label } from './styles';
 
 export default function Login({ navigation }) {
+    const dispatch = useDispatch();
+
+    async function login() {
+        let AuthorizationContent
+
+        try {
+            AuthorizationContent = await getToken();
+        } catch (error) {
+            Alert.alert('Erro ao logar com o Spotify', 'Tente novamente mais tarde!');
+            return
+        }
+
+        const {
+            access_token,
+            refresh_token,
+            expires_in
+        } = AuthorizationContent;
+
+        const expirationTime = new Date().getTime() + expires_in * 1000;
+        // console.tron.warn(expirationTime)
+        dispatch(signInSuccess(access_token, expirationTime, refresh_token))
+        return
+    }
+
     return (
         <Background>
             <Container>
-                <TouchableContainer onPress={() => { navigation.navigate('Home') }}>
+                <TouchableContainer onPress={login}>
                     <MaterialCommunityIcons name="spotify" size={32} color="#81b71a" />
                     <Label>Login com Spotify</Label>
                 </TouchableContainer>
