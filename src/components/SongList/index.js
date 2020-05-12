982a1ef6abe2de3ad7b0fe7ff6e493b886e0582a
Matheus Morais <matheus.silva.morais01@gmail.com>
2020-05-11 23:18:44 -0300

    fix: agora é possivel colocar para tocar uma musica especifica de dentro da playlist

--- src/components/SongList/index.js
+++ src/components/SongList/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { returnToHome } from '../../store/modules/playlist/action';
+import {
+    selectPlaylist,
+    playTheMusic,
+    playOrPauseTheMusic
+} from '../../store/modules/playlist/action';
 
 import {
     Container,
@@ -12,17 +17,46 @@ import {
     TouchableIcon
 } from './styles';
 
-const SongList = ({ data }) => {
+const SongList = ({ data, index }) => {
+    const dispatch = useDispatch()
+
+    const currentSongPlaying = useSelector(state => state.playlist.currentSongPlaying)
+    const indexCurrentSongPlaying = useSelector(state => state.playlist.indexCurrentSongPlaying)
+    const songsPlaying = useSelector(state => state.playlist.songsPlaying)
+    const indexCurrentPlayList = useSelector(state => state.playlist.indexCurrentPlayList)
+    const isMusicPaused = useSelector(state => state.playlist.isMusicPaused)
+
+    const iconName = currentSongPlaying && data.track.id === currentSongPlaying.track.id ? (isMusicPaused ? 'play' : 'pause') : 'play';
+    const itemColor = currentSongPlaying && data.track.id === currentSongPlaying.track.id ? '#81b71a' : '#fff';
+
+    function handleSong() {
+        if (isMusicPaused) {
+            if (index === indexCurrentSongPlaying) {
+                dispatch(playOrPauseTheMusic(false))
+            } else {
+                dispatch(playOrPauseTheMusic(false))
+                dispatch(playTheMusic(songsPlaying, data, index, indexCurrentPlayList))
+            }
+        } else {
+            if (currentSongPlaying && index === indexCurrentSongPlaying) {
+                dispatch(playOrPauseTheMusic(true))
+            } else {
+                dispatch(playOrPauseTheMusic(false))
+                dispatch(playTheMusic(songsPlaying, data, index, indexCurrentPlayList))
+            }
+        }
+    }
+
     return (
         <Container>
-            <ContainerTracks onPress={() => { }}>
+            <ContainerTracks onPress={handleSong}>
                 <ContainerDescription>
                     <Name>{data.track.name}</Name>
                     <Album>{data.track.album.name}</Album>
                 </ContainerDescription>
             </ContainerTracks>
-            <TouchableIcon onPress={() => { }}>
-                <MaterialCommunityIcons name="play" size={32} color="#fff" />
+            <TouchableIcon onPress={handleSong}>
+                <MaterialCommunityIcons name={iconName} size={32} color={itemColor} />
             </TouchableIcon>
         </Container>
     );
